e1d48278db0b6bbb20d74c01e6491daebf6e11cf
Onabajo Oluwakeji Daniel <62175369+OnabajoOluwakeji@users.noreply.github.com>
2022-07-21 14:33:05 +0100

    update

---
+++ src/container/burger/Burger.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Aux from '../../hoc/Auxilliary'
+
+const Burger = () => {
+  return (
+    <Aux>
+    <div>Burger</div>
+    <div>Burger control</div>
+    </Aux>
+  )
+}
+
+export default Burger
---
+++ src/hoc/Auxilliary.js
@@ -0,0 +1,6 @@
+
+export const aux = (props) => {
+  return props.children;
+}
+
+export default aux;
